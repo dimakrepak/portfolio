a68f314f7046d694d7e69a2a9d5b9e91e515974d
Dmitry Krepak <krepakdima@gmail.com>
2021-09-17 02:37:59 +0300

    .

--- server/app.js
+++ server/app.js
@@ -10,11 +10,11 @@ if (process.env.NODE_ENV === "production") {
 //Connection to client side
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
-  app.use(express.static(__dirname, "client", "build"));
+  app.use(express.static(__dirname, "../client", "build"));
   // Express serve up index.html file if it doesn't recognize routeget
   const path = require("path");
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
