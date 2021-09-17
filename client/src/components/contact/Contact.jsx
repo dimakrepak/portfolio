e070045bdb4da9c88d2ba98d3bcfc3f6d33d3e2c
Dmitry Krepak <krepakdima@gmail.com>
2021-09-17 21:49:43 +0300

    .

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,6 +1,29 @@
 import "./contact.scss";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Contact() {
+  const [contact, setContact] = useState({ name: "", email: "", message: "" });
+
+  function handleFormChange(e) {
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value,
+    });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await axios({
+        method: "POST",
+        url: "/api/email",
+        data: contact,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  console.log(contact);
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -8,9 +31,24 @@ export default function Contact() {
       </div>
       <div className="right">
         <h1>Get in touch.</h1>
-        <form>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            onChange={handleFormChange}
+          />
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleFormChange}
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            onChange={handleFormChange}
+          ></textarea>
           <button type="submit">Send</button>
         </form>
       </div>
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 5000;
+const router = require("./routes/routes.js");
+
+//Middleware
+app.use(express.json());
+app.use("/api", router);
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Server started " + port);
+});
