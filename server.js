9669e465f0614076e4141fc43f5670eab6127f9f
Dmitry Krepak <krepakdima@gmail.com>
2021-09-17 23:45:40 +0300

    .

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
-const router = require("./routes/routes.js");
+const router = require("./server/routes/routes");
 
 //Middleware
 app.use(express.json());
