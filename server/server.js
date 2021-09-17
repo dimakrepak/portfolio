8d30d75a562db0a573b85d963a6c5361cbc415c7
Dmitry Krepak <krepakdima@gmail.com>
2021-09-17 23:27:40 +0300

    .

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const router = require("./routes/routes.js");
 app.use(express.json());
 app.use("/api", router);
 // Serve static files from the React app
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "\\client\\build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "../client/build/index.html"));
+  res.sendFile(path.join(__dirname + "\\client\\build\\index.html"));
 });
 
 app.listen(port, () => {
